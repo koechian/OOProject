deaa22ff91c4383935db7d42debe3c59a70d943d
Ian Koech <ianovic8@live.com>
2021-09-12 15:09:51 +0300

    Tasks int

---
+++ src/main/java/com/example/application/views/tasks/Tasks.java
@@ -0,0 +1,45 @@
+package com.example.application.views.tasks;
+
+public class Tasks {
+
+    private String unit;
+    private String task;
+    private String date;
+    private String done;
+
+    public Tasks() {
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getDone() {
+        return done;
+    }
+
+    public void setDone(String done) {
+        this.done = done;
+    }
+
+}
--- src/main/java/com/example/application/views/tasks/TasksView.java
+++ src/main/java/com/example/application/views/tasks/TasksView.java
@@ -3,10 +3,10 @@ package com.example.application.views.tasks;
 import java.util.Arrays;
 import java.util.List;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -21,138 +21,96 @@ import com.example.application.views.MainLayout;
 @PageTitle("Tasks")
 @Route(value = "empty1", layout = MainLayout.class)
 public class TasksView extends Div implements AfterNavigationObserver {
+        Button addButton = new Button("Add");
 
-    Grid<Person> grid = new Grid<>();
+        Grid<Tasks> grid = new Grid<>();
 
-    public TasksView() {
-        addClassName("tasks-view");
-        setSizeFull();
-        grid.setHeight("100%");
-        grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
-        grid.addComponentColumn(person -> createCard(person));
-        add(grid);
-    }
+        public TasksView() {
+                addClassName("tasks-view");
+                setSizeFull();
+                grid.setHeight("100%");
+                grid.addThemeVariants(GridVariant.LUMO_NO_BORDER, GridVariant.LUMO_NO_ROW_BORDERS);
+                grid.addComponentColumn(person -> createCard(person));
+                add(grid);
+        }
 
-    private HorizontalLayout createCard(Person person) {
-        HorizontalLayout card = new HorizontalLayout();
-        card.addClassName("card");
-        card.setSpacing(false);
-        card.getThemeList().add("spacing-s");
+        private HorizontalLayout createCard(Tasks person) {
+                HorizontalLayout card = new HorizontalLayout();
+                card.addClassName("card");
+                card.setSpacing(false);
+                card.getThemeList().add("spacing-s");
 
-        Image image = new Image();
-        image.setSrc(person.getImage());
-        VerticalLayout description = new VerticalLayout();
-        description.addClassName("description");
-        description.setSpacing(false);
-        description.setPadding(false);
+                VerticalLayout description = new VerticalLayout();
+                description.addClassName("description");
+                description.setSpacing(false);
+                description.setPadding(false);
 
-        HorizontalLayout header = new HorizontalLayout();
-        header.addClassName("header");
-        header.setSpacing(false);
-        header.getThemeList().add("spacing-s");
+                HorizontalLayout header = new HorizontalLayout();
+                header.addClassName("header");
+                header.setSpacing(false);
+                header.getThemeList().add("spacing-s");
 
-        Span name = new Span(person.getName());
-        name.addClassName("name");
-        Span date = new Span(person.getDate());
-        date.addClassName("date");
-        header.add(name, date);
+                Span unit = new Span(person.getUnit());
+                unit.addClassName("unit");
+                Span date = new Span(person.getDate());
+                date.addClassName("date");
+                header.add(unit, date);
 
-        Span post = new Span(person.getPost());
-        post.addClassName("post");
+                Span task = new Span(person.getTask());
+                task.addClassName("post");
 
-        HorizontalLayout actions = new HorizontalLayout();
-        actions.addClassName("actions");
-        actions.setSpacing(false);
-        actions.getThemeList().add("spacing-s");
+                HorizontalLayout actions = new HorizontalLayout();
+                actions.addClassName("actions");
+                actions.setSpacing(false);
+                actions.getThemeList().add("spacing-s");
 
-        Icon likeIcon = VaadinIcon.HEART.create();
-        likeIcon.addClassName("icon");
-        Span likes = new Span(person.getLikes());
-        likes.addClassName("likes");
-        Icon commentIcon = VaadinIcon.COMMENT.create();
-        commentIcon.addClassName("icon");
-        Span comments = new Span(person.getComments());
-        comments.addClassName("comments");
-        Icon shareIcon = VaadinIcon.CONNECT.create();
-        shareIcon.addClassName("icon");
-        Span shares = new Span(person.getShares());
-        shares.addClassName("shares");
+                Icon likeIcon = VaadinIcon.HEART.create();
+                likeIcon.addClassName("icon");
+                Span likes = new Span(String.valueOf(person.getDone()));
+                likes.addClassName("likes");
 
-        actions.add(likeIcon, likes, commentIcon, comments, shareIcon, shares);
+                actions.add(likeIcon, likes);
 
-        description.add(header, post, actions);
-        card.add(image, description);
-        return card;
-    }
+                description.add(header, task, actions);
+                card.add(description);
+                return card;
+        }
 
-    @Override
-    public void afterNavigation(AfterNavigationEvent event) {
+        @Override
+        public void afterNavigation(AfterNavigationEvent event) {
 
-        // Set some data when this view is displayed.
-        List<Person> persons = Arrays.asList( //
-                createPerson("https://randomuser.me/api/portraits/men/42.jpg", "John Smith", "May 8",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/42.jpg", "Abagail Libbie", "May 3",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/24.jpg", "Alberto Raya", "May 3",
+                // Set some data when this view is displayed.
+                List<Tasks> tasks = Arrays.asList( //
+                                createTask("OOP", "May 8", "Read for exams", "N0T DONE"),
+                                createTask("OOP", "Sep 13", "Complete project", "DONE"),
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/24.jpg", "Emmy Elsner", "Apr 22",
+                                createTask("COA", "May 3", "GROUP WORK", "NOT DONE"), createTask("DSA", "May 3",
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/76.jpg", "Alf Huncoot", "Apr 21",
+                                                "PROJECT", "DONE"),
+                                createTask("World Civ", "Apr 22",
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/76.jpg", "Lidmila Vilensky", "Apr 17",
+                                                "ASSIGNMENT", "NOT DONE"),
+                                createTask("Ethics", "Sep 21",
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/94.jpg", "Jarrett Cawsey", "Apr 17",
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/94.jpg", "Tania Perfilyeva", "Mar 8",
+                                                "Pick papers", "NOT DONE"),
+                                createTask("Digital Logic", "Nov 17",
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/16.jpg", "Ivan Polo", "Mar 5",
+                                                "Special exams", "NOT DONE")
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/16.jpg", "Emelda Scandroot", "Mar 5",
+                );
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/men/67.jpg", "Marcos Sá", "Mar 4",
+                grid.setItems(tasks);
+        }
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20"),
-                createPerson("https://randomuser.me/api/portraits/women/67.jpg", "Jacqueline Asong", "Mar 2",
+        private static Tasks createTask(String unit, String date, String task, String done) {
+                Tasks p = new Tasks();
 
-                        "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document without relying on meaningful content (also called greeking).",
-                        "1K", "500", "20")
+                p.setUnit(unit);
+                p.setDate(date);
+                p.setTask(task);
+                p.setDone(done);
 
-        );
-
-        grid.setItems(persons);
-    }
-
-    private static Person createPerson(String image, String name, String date, String post, String likes,
-            String comments, String shares) {
-        Person p = new Person();
-        p.setImage(image);
-        p.setName(name);
-        p.setDate(date);
-        p.setPost(post);
-        p.setLikes(likes);
-        p.setComments(comments);
-        p.setShares(shares);
-
-        return p;
-    }
+                return p;
+        }
 
 }
